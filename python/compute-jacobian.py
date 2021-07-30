bb77b572f73a75afc7e5573850e0706ca0537797
Kévin SPINICCI <kevin.spinicci@outlook.com>
2021-07-30 13:52:29 +0200

    Complete ODEs

--- python/compute-jacobian.py
+++ python/compute-jacobian.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python
-from math import exp, tan
 from sympy import Matrix, MatrixSymbol
 from sympy.core.symbol import Symbol, symbols
 from sympy import tanh
 from sympy.codegen.cfunctions import log10, Pow
 from sympy.printing.c import C99CodePrinter
 import re
-
-from sympy.printing.codeprinter import ccode
 
 def replace_x(line):
     pat = "X\[(\d+),(\d+)\]"
@@ -35,6 +32,7 @@ m_CO2_C,m_H_C,m_HCO3_C,m_CO2_c_old, m_H_c, m_HCO3_c = symbols('m_CO2_C,m_H_C,m_H
 
 X = MatrixSymbol('x', 8, 1)
 S = MatrixSymbol('S', 5, 5)
+Ph = Symbol('Ph')
 gamma_array = MatrixSymbol('gamma_array', 5, 5)
 M1 = Matrix([
     [A * HIF_PROD - D * X[0] * ((Pow(S[0, 1], N) / (Pow(S[0, 1], N) + Pow(O, N))) + gamma_array[0, 1] * (Pow(O, N) / (Pow(S[0, 1], N) + Pow(O, N)))) ],
@@ -43,7 +41,7 @@ M1 = Matrix([
     [A * ((Pow(S[3, 4], N) / (Pow(S[3, 4], N) + Pow(X[2], N))) + gamma_array[3, 4] * (Pow(X[2], N) / (Pow(S[3, 4], N) + Pow(X[2], N)))) - D * X[3] ],
     [0.0],
     [SensO2 * q_O2 * MW_CO2 / MW_O2 - k1 * m_CO2_C + k2 * m_H_C * m_HCO3_C * 1000 * MW_CO2 / (V_C * MW_H * MW_HCO3) + PM_CO2 * (m_CO2_c_old / V_c-m_CO2_C / (V_C)) * (S_C)],
-    [SensATP * gAcL * MW_H / MW_AcL + k1 * m_CO2_C * MW_H / MW_CO2 - k2 * m_H_C * m_HCO3_C * 1000 / (V_C * MW_HCO3) - (2.0 - tanh(c2aH_slope * (-log10(1000 * m_H_C / (V_C * MW_H))) - c2aH_thr)) * VMAXAcL * MW_H / MW_AcL * (S_C) * m_H_C / ((V_C) * K_mAcL * MW_H / MW_AcL + m_H_C) + (2.0 - tanh(a2cH_slope * (-log10(1000 * m_H_c / (V_c * MW_H))) - a2cH_thr)) * VMAXAcL * MW_H / MW_AcL * (S_C) * m_H_c / (V_c * K_mAcL * MW_H / MW_AcL + m_H_c) - SensO2 * SensATP * 0.5 * (1.0 + ((-log10(1000 * m_H_c / (V_c * MW_H))) - pH0_NHE) / (l_NHE + abs((-log10(1000 * m_H_c / (V_c * MW_H))) - pH0_NHE))) * VMAXNHE * (S_C) * Pow(m_H_C, a) / (Pow(V_C * MW_H * K_mNHE / 1000, a) + Pow(m_H_C, a))],
+    [SensATP * Ph * V_C * 1e-6 + k1 * m_CO2_C * MW_H / MW_CO2 - k2 * m_H_C * m_HCO3_C * 1000 / (V_C * MW_HCO3) - (2.0 - tanh(c2aH_slope * (-log10(1000 * m_H_C / (V_C * MW_H))) - c2aH_thr)) * VMAXAcL * MW_H / MW_AcL * (S_C) * m_H_C / ((V_C) * K_mAcL * MW_H / MW_AcL + m_H_C) + (2.0 - tanh(a2cH_slope * (-log10(1000 * m_H_c / (V_c * MW_H))) - a2cH_thr)) * VMAXAcL * MW_H / MW_AcL * (S_C) * m_H_c / (V_c * K_mAcL * MW_H / MW_AcL + m_H_c) - SensO2 * SensATP * 0.5 * (1.0 + ((-log10(1000 * m_H_c / (V_c * MW_H))) - pH0_NHE) / (l_NHE + abs((-log10(1000 * m_H_c / (V_c * MW_H))) - pH0_NHE))) * VMAXNHE * (S_C) * Pow(m_H_C, a) / (Pow(V_C * MW_H * K_mNHE / 1000, a) + Pow(m_H_C, a))],
     [k1 * MW_HCO3 / MW_CO2 * m_CO2_C - k2 * m_H_C * m_HCO3_C * 1000 / (V_C * MW_H) + SensATP * (0.5) * (1.0 + tanh(l_THCO3 * (-log10(1000 * m_H_c / (V_c * MW_H)) - pHe0_THCO3))) * (0.5) * (1.0 +  tanh(g_THCO3 * (pHi0_THCO3 - (-log10(1000 * m_H_C / (V_C * MW_H)))))) * VMAXTHCO3 * (S_C) * m_HCO3_c / (V_c * K_mTHCO3 * MW_HCO3 / 1000 + m_HCO3_c)]
 ])
 M2 = Matrix([
@@ -80,15 +78,31 @@ cpp_code = '''void jacobi ( const state_type &x , matrix_type &J , const double 
 	auto S = p->S;
 	auto gamma_array = p->gamma_array;
 
+	const double m_CO2_C = x[5];
+    const double m_H_C = x[6];
+    const double m_HCO3_C = x[7];
+    const double m_H_c = nearest_gradient[microenvironment.find_density_index("proton")]; 
+	const double m_HCO3_c = nearest_gradient[microenvironment.find_density_index("HCO3")];
+	const double m_CO2_c_old = nearest_gradient[microenvironment.find_density_index("CO2")];
+	const double O = nearest_gradient[microenvironment.find_density_index("oxygen")];
+	const double V_c = microenvironment.mesh.dV;
+	const double V_C = pCell->phenotype.volume.total;
+	const double S_C = pCell->phenotype.geometry.surface_area;
+
 '''
 i=0
 for i in range(Jacobian.shape[0]):
     for j in range(Jacobian.shape[1]):
         cpp_code = cpp_code + "\t" + my_printer.doprint(Jacobian[i,j], assign_to=f'J({i},{j})') + '\n'
+
+for i in range(Jacobian.shape[0]):
+    cpp_code = cpp_code + "\t" + f'dfdt[{i}]=0.0;' + '\n'
+
 cpp_code = cpp_code + "}"
 
 with open('jacobi.cpp', 'w') as out:
     out.write(cpp_code)
+
 
 # Just In case
     # [
