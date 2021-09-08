4f957b0de438767ff9a24b99334f7ea6f1d753d7
Kévin SPINICCI <kevin.spinicci@outlook.com>
2021-09-08 16:17:47 +0200

    GSEA Worflow

--- PDCL-Enrichment/gsea-analysis.R
+++ PDCL-Enrichment/gsea-analysis.R
@@ -1,48 +1,24 @@
+setwd("/home/spinicck/PhD/")
+
 ######### Remove every variable in memory
 rm(list = ls())
 
 ######## Load Library needed
 library(fgsea)
-library(stringr)
-
-######### Source all functions
-rfun_dir <- "~/Bureau/EnrichmentAnalysis/Script/R/function/"
-for(f in list.files(rfun_dir)) {
-  source(paste0(rfun_dir, f, ""))
-}
-rm(f, rfun_dir)
+library(dplyr)
 
 #IMPORTANT!! We must choose a seed to ensure the analysis is reproducible. This is because GSEA uses a 
 #permutation test to span the null distribution of the statistic. Hence, each time we run it, results are 
 #expected to change slightly.
 set.seed(1)
 
-reactome.gmt <- gmtPathways("/home/spinicck/PhD/Data/gene-set/reactome_pathways_symbol.gmt")
-bioplanet.gmt <- gmtPathways("/home/spinicck/PhD/Data/gene-set/bioplanet_pathways_symbol.gmt")
+reactome.gmt <- gmtPathways("Data/gene-set/reactome_pathways_symbol.gmt")
+bioplanet.gmt <- gmtPathways("Data/gene-set/bioplanet_pathways_symbol.gmt")
 
-######### Load all DE Analysis Results
-deseq2.res.dir <- "/home/spinicck/PhD/Data/PDCL/deseq2-analysis/"
-deseq2.res.files <- list.files(deseq2.res.dir)
-deseq2.res.files <- paste0(deseq2.res.dir, deseq2.res.files)
-limma.res.dir <- "/home/spinicck/PhD/Data/PDCL/limma/"
-limma.res.files <- list.files(limma.res.dir)
-limma.res.files <- paste0(limma.res.dir, limma.res.files)
-res.de.files <- c(deseq2.res.files, limma.res.files)
-
-####### Function for creating a ranked list of genes
-create.ranked.gene.list <- function(genes.dt){
-  genes.dt <- na.omit(genes.dt)
-  genes.dt <- genes.dt[genes.dt$padj<1,]
-  ranked <- genes.dt$padj
-  names(ranked) <- row.names(genes.dt)
-  return(ranked)
-}
-
-######### Run GSEA Analysis
-gseares <- lapply(res.de.files, function(f){
+# run gsea analysis with reactome and bioplanet GMT files for a list of DE analysis result
+run.gsea <- function(f, reactome.gmt, bioplanet.gmt, create.ranked.list){
   message("###### GSEA Analysis for : ", f)
-  genes.dt <- read.table(f, header = T, row.names = 1, sep = ",")
-  ranked.list <- create.ranked.gene.list(genes.dt)
+  ranked.list <- create.ranked.list(f)
   res <- list()
   message("\tRunning Reactome Enrichment ...")
   res[["reactome"]] <- fgsea(reactome.gmt, ranked.list, minSize=15, maxSize=500, nperm = 1000)
@@ -51,20 +27,55 @@ gseares <- lapply(res.de.files, function(f){
   res[["bioplanet"]] <- fgsea(bioplanet.gmt, ranked.list, minSize=15, maxSize=500, nperm = 1000)
   message("\tDone !")
   return(res)
-})
+}
 
-analysis.name <- sub("\\.\\w+", "", res.de.files, perl = T)
-analysis.name <- sub("/.+/(deseq2|limma)_", "", analysis.name, perl = T, ignore.case = T)
-names(gseares) <- analysis.name
-
-######### Save Result to Enrichment Files
-save.dir <- "/home/spinicck/PhD/Data/PDCL/gsea/"
-for (analysis in names(gseares) ) {
-  for (db.source in names(gseares[[analysis]]) ){
-    f <- paste0(save.dir, "gsea_", analysis, "_", db.source, ".csv")
-    enrichment.res <- gseares[[analysis]][[db.source]]
-    enrichment.res$GeneList <- sapply(enrichment.res$leadingEdge, function(x){ paste0(x, collapse = ", ")})
-    enrichment.res <- enrichment.res[, !"leadingEdge" ]
-    write.table(enrichment.res, file = f, row.names = F, col.names = T, sep = "\t")
+# Save a list of Generic Enrichment Map to tab delimited txt files
+# Files name conttain the tool used for differential expression analysis, which patient
+# has been compared to the control (analysis), the source of the gmt file (db.source)
+save.gem.list.to.txt <- function(gem.list, save.dir, de.analysis){
+  for (analysis in names(gem.list) ) {
+    for (db.source in names(gem.list[[analysis]]) ){
+      f <- paste0(save.dir, "gsea_", de.analysis, "_",analysis, "_", db.source, ".csv")
+      gem.dt <- as.data.frame(gem.list[[analysis]][[db.source]]) %>% mutate(leadingEdge=sapply(leadingEdge, function(e){ paste0(e, collapse = ", ")}))
+      write.table(gem.dt, file = f, row.names = F, col.names = T, sep = "\t")
+    }
   }
 }
+
+# Remove path and file extension from the file name and return only the analysis name
+# which is in the form : patientID_vs_control
+get.analysis.name <- function(file.name){
+  analysis.name <- sub("\\.\\w+", "", file.name, perl = T, ignore.case = T)
+  analysis.name <- sub(".+/(deseq2|limma)_", "", analysis.name, perl = T, ignore.case = T)
+  return(analysis.name)
+}
+
+######### Run GSEA for DESeq2 result
+deseq2.res.dir <- "Result/PDCL/deseq2/"
+deseq2.res.files <- list.files(deseq2.res.dir)
+deseq2.res.files <- paste0(deseq2.res.dir, deseq2.res.files)
+deseq2.gsea <- lapply(deseq2.res.files[1], run.gsea, reactome.gmt, bioplanet.gmt, function(f) { 
+  x <- read.table(f, header = T, sep = ",")
+  x <- as.data.frame(x) %>% filter(padj<0.1 & !is.na(padj)) %>% transmute(rank=log2FoldChange, gene=X) %>% arrange(desc(rank))
+  rnk <- x$rank
+  names(rnk) <- x$gene
+  rnk
+})
+names(deseq2.gsea) <- sapply(deseq2.res.files, get.analysis.name)
+saveRDS(deseq2.gsea, file = "Result/PDCL/gsea_deseq2_genes.rds")
+save.gem.list.to.txt(deseq2.gsea, "Result/PDCL/gsea/deseq2/", "deseq2")
+
+######### Run GSEA for Limma result
+limma.res.dir <- "Result/PDCL/limma/"
+limma.res.files <- list.files(limma.res.dir)
+limma.res.files <- paste0(limma.res.dir, limma.res.files)
+limma.gsea <- lapply(limma.res.files, run.gsea, reactome.gmt, bioplanet.gmt, function(f) { 
+  x <- read.table(f, header = T, sep = ",")
+  x <- as.data.frame(x) %>% filter(adj.P.Val<0.1 & !is.na(adj.P.Val)) %>% transmute(rank=logFC, gene=X) %>% arrange(desc(rank))
+  rnk <- x$rank
+  names(rnk) <- x$gene
+  rnk
+})
+names(limma.gsea) <- sapply(limma.res.files, get.analysis.name)
+saveRDS(limma.gsea, file = "Result/PDCL/gsea_limma_genes.rds")
+save.gem.list.to.txt(limma.gsea, "Result/PDCL/gsea/limma/", "limma")
