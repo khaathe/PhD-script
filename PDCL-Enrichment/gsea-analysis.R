82b467065cc0d2a5344710d889f1425fc60a6d2e
Kévin SPINICCI <kevin.spinicci@outlook.com>
2021-09-30 14:57:15 +0200

    Drop genes filtering on pvalue + begin function to create GEM file ready for cytoscape

--- PDCL-Enrichment/gsea-analysis.R
+++ PDCL-Enrichment/gsea-analysis.R
@@ -12,8 +12,8 @@ library(dplyr)
 #expected to change slightly.
 set.seed(1)
 
-reactome.gmt <- gmtPathways("Data/gene-set/reactome_pathways_symbol.gmt")
-bioplanet.gmt <- gmtPathways("Data/gene-set/bioplanet_pathways_symbol.gmt")
+reactome.gmt <- gmtPathways("Data/gene-set/jorge-gmt/reactome_gmt_symbol_no_unwanted_categ_no_less_10.gmt")
+bioplanet.gmt <- gmtPathways("Data/gene-set/jorge-gmt/bioplanet_gmt_symbol_no_unwanted_categ_no_less_10.gmt")
 
 # run gsea analysis with reactome and bioplanet GMT files for a list of DE analysis result
 run.gsea <- function(f, reactome.gmt, bioplanet.gmt, create.ranked.list){
@@ -35,9 +35,9 @@ run.gsea <- function(f, reactome.gmt, bioplanet.gmt, create.ranked.list){
 save.gem.list.to.txt <- function(gem.list, save.dir, de.analysis){
   for (analysis in names(gem.list) ) {
     for (db.source in names(gem.list[[analysis]]) ){
-      f <- paste0(save.dir, "gsea_", de.analysis, "_",analysis, "_", db.source, ".csv")
+      f <- paste0(save.dir, "gsea_", de.analysis, "_",analysis, "_", db.source, ".txt")
       gem.dt <- as.data.frame(gem.list[[analysis]][[db.source]]) %>% mutate(leadingEdge=sapply(leadingEdge, function(e){ paste0(e, collapse = ", ")}))
-      write.table(gem.dt, file = f, row.names = F, col.names = T, sep = "\t")
+      write.table(gem.dt, file = f, row.names = F, col.names = T, sep = "\t", quote=F)
     }
   }
 }
@@ -50,13 +50,23 @@ get.analysis.name <- function(file.name){
   return(analysis.name)
 }
 
+# Convert a list of GSEA Analysis result to a list of Data Frame containing the column in the Generic Enrichment Map (GEM)
+# as described in GEM documentation (https://enrichmentmap.readthedocs.io/en/latest/FileFormats.html#generic-results-files)
+convert.gsea.to.gem <- function(res){
+  lapply(res, function(x){
+    x$result %>% dplyr::rename(pathway_id = term_id, description = term_name, genes = intersection) %>%
+      mutate(fdr = p_value, phenotype = 1) %>%
+      select(pathway_id,description, p_value, fdr, phenotype, genes)
+  })
+}
+
 ######### Run GSEA for DESeq2 result
 deseq2.res.dir <- "Result/PDCL/deseq2/"
 deseq2.res.files <- list.files(deseq2.res.dir)
 deseq2.res.files <- paste0(deseq2.res.dir, deseq2.res.files)
-deseq2.gsea <- lapply(deseq2.res.files[1], run.gsea, reactome.gmt, bioplanet.gmt, function(f) { 
+deseq2.gsea <- lapply(deseq2.res.files, run.gsea, reactome.gmt, bioplanet.gmt, function(f) { 
   x <- read.table(f, header = T, sep = ",")
-  x <- as.data.frame(x) %>% filter(padj<0.1 & !is.na(padj)) %>% transmute(rank=log2FoldChange, gene=X) %>% arrange(desc(rank))
+  x <- as.data.frame(x) %>% transmute(rank=log2FoldChange, gene=X) %>% arrange(desc(rank))
   rnk <- x$rank
   names(rnk) <- x$gene
   rnk
@@ -71,7 +81,7 @@ limma.res.files <- list.files(limma.res.dir)
 limma.res.files <- paste0(limma.res.dir, limma.res.files)
 limma.gsea <- lapply(limma.res.files, run.gsea, reactome.gmt, bioplanet.gmt, function(f) { 
   x <- read.table(f, header = T, sep = ",")
-  x <- as.data.frame(x) %>% filter(adj.P.Val<0.1 & !is.na(adj.P.Val)) %>% transmute(rank=logFC, gene=X) %>% arrange(desc(rank))
+  x <- as.data.frame(x) %>% transmute(rank=logFC, gene=X) %>% arrange(desc(rank))
   rnk <- x$rank
   names(rnk) <- x$gene
   rnk
