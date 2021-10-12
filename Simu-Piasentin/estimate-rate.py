d603f3e9f1168154e860c20af4a123cd36bd75aa
Kévin SPINICCI <kevin.spinicci@outlook.com>
2021-10-12 12:17:12 +0200

    Solving pH ode

--- Simu-Piasentin/estimate-rate.py
+++ Simu-Piasentin/estimate-rate.py
@@ -39,7 +39,7 @@ SensATP =  1.0
 R_cell = 8.41
 V_cell = 4/3 * math.pi * math.pow(R_cell, 3)
 S_cell = 4 * math.pi * math.pow(R_cell, 2)
-V_extracell = 2e11
+V_extracell = 8000
 
 """
     VMAXAcL = 0.0057480000000000005
@@ -62,65 +62,68 @@ x = [CO2_intra_initial, H_intra_initial,
     HCO3_intra_initial, CO2_extra_initial, H_extra_initial, HCO3_extra_initial]
 
 def ph_ode (t, x, VMAXAcL, VMAXNHE, VMAXTHCO3, VMAXCA9):
-    dxdt = []
-    diff_CO2_in_out = PM_CO2 * ( (x[4] / V_extracell) - (x[5] / V_cell) ) * S_cell
+    dxdt = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    diff_CO2_in_out = PM_CO2 * ( (x[3] / V_extracell) - (x[0] / V_cell) ) * S_cell
 	
-    nu_MCT_in_out = {
-        (2.0 - math.tanh(c2aH_slope * (-math.log10(1000 * x[6] / (V_cell * MW_H))) - c2aH_thr)) * VMAXAcL * MW_H / MW_AcL 
-        * (S_cell) * x[6] / ((V_cell) * K_mAcL * MW_H / MW_AcL + x[6])
-    }
+    nu_MCT_in_out = (
+        (2.0 - math.tanh(c2aH_slope * (-math.log10(1000 * x[1] / (V_cell * MW_H))) - c2aH_thr)) * VMAXAcL * MW_H / MW_AcL 
+        * (S_cell) * x[1] / ((V_cell) * K_mAcL * MW_H / MW_AcL + x[1])
+    )
         
-    nu_MCT_out_in = {
-        (2.0 - math.tanh(a2cH_slope * (-math.log10(1000 * x[5] / (V_extracell * MW_H))) - a2cH_thr)) * VMAXAcL * MW_H / MW_AcL 
-        * (S_cell) * x[5] / (V_extracell * K_mAcL * MW_H / MW_AcL + x[5])
-    }
+    nu_MCT_out_in = (
+        (2.0 - math.tanh(a2cH_slope * (-math.log10(1000 * x[4] / (V_extracell * MW_H))) - a2cH_thr)) * VMAXAcL * MW_H / MW_AcL 
+        * (S_cell) * x[4] / (V_extracell * K_mAcL * MW_H / MW_AcL + x[4])
+    )
         
-    nu_NHE_in_out = {
-        SensO2 * SensATP * 0.5 * (1.0 + ((-math.log10(1000 * x[5] / (V_extracell * MW_H))) - pH0_NHE) / (l_NHE + abs((-math.log10(1000 * x[5] / (V_extracell * MW_H))) - pH0_NHE))) 
-        * VMAXNHE * (S_cell) * pow(x[6], a) / (pow(V_cell * MW_H * K_mNHE / 1000, a) + pow(x[6], a))
-    }
+    nu_NHE_in_out = (
+        SensO2 * SensATP * 0.5 * (1.0 + ((-math.log10(1000 * x[4] / (V_extracell * MW_H))) - pH0_NHE) / (l_NHE + abs((-math.log10(1000 * x[4] / (V_extracell * MW_H))) - pH0_NHE))) 
+        * VMAXNHE * (S_cell) * pow(x[1], a) / (pow(V_cell * MW_H * K_mNHE / 1000, a) + pow(x[1], a))
+    )
         
-    nu_THCO3_out_in = {
+    nu_THCO3_out_in = (
         SensATP * (0.5) * 
-        (1.0 + math.tanh(l_THCO3 * (-math.log10(1000 * x[5] / (V_extracell * MW_H)) - pHe0_THCO3))) * (0.5) 
+        (1.0 + math.tanh(l_THCO3 * (-math.log10(1000 * x[4] / (V_extracell * MW_H)) - pHe0_THCO3))) * (0.5) 
         * (1.0 + math.tanh(g_THCO3 * (pHi0_THCO3 - (-math.log10(1000 * x[2] / (V_cell * MW_H)))))) 
-        * VMAXTHCO3 * (S_cell) * x[6] / (V_extracell * K_mTHCO3 * MW_HCO3 / 1000 + x[6])
-    }
+        * VMAXTHCO3 * (S_cell) * x[5] / (V_extracell * K_mTHCO3 * MW_HCO3 / 1000 + x[5])
+    )
     
-    nu_CA9 = (3.0 + 2.0 * math.tanh(-d_CA9 * SensO2)) * VMAXCA9 * S_cell * x[4] / (V_extracell * K_mCA9 * MW_CO2 / 1000 + x[4])
+    nu_CA9 = (3.0 + 2.0 * math.tanh(-d_CA9 * SensO2)) * VMAXCA9 * S_cell * x[3] / (V_extracell * K_mCA9 * MW_CO2 / 1000 + x[3])
 
     # intracellular carbondioxide dynamics
-    dxdt[0] = {
+    dxdt[0] = (
         SensO2 * q_O2 * MW_CO2 / MW_O2 # internal rate
-            -k1 * x[0] + k2 * x[2] * x[3] * 1000 * MW_CO2 / (V_cell * MW_H * MW_HCO3) # chemical equilibrium
+            -k1 * x[0] + k2 * x[1] * x[2] * 1000 * MW_CO2 / (V_cell * MW_H * MW_HCO3) # chemical equilibrium
             + diff_CO2_in_out
-    }
+    )
             
 
     # intracellular hydrogen dynamics
-    dxdt[1] = {
+    dxdt[1] = (
         SensATP * gAcL * MW_H / MW_AcL * MW_H * V_cell * 1e-6 # internal rate
-        + k1 * x[0] * MW_H / MW_CO2 - k2 *x[2] * x[3] * 1000 / (V_cell * MW_HCO3) # chemical equilibrium
+        + k1 * x[0] * MW_H / MW_CO2 - k2 *x[1] * x[2] * 1000 / (V_cell * MW_HCO3) # chemical equilibrium
         - nu_MCT_in_out + nu_MCT_out_in - nu_NHE_in_out
-    }
+    )
 
     # intracellular bicarbonate ions dynamics
-    dxdt[2] = {
-        k1 * MW_HCO3 / MW_CO2 * x[0] - k2 * x[2] * x[3] * 1000 / (V_cell * MW_H) # chemical equilibrium
+    dxdt[2] = (
+        k1 * MW_HCO3 / MW_CO2 * x[0] - k2 * x[1] * x[2] * 1000 / (V_cell * MW_H) # chemical equilibrium
         + nu_THCO3_out_in
-    }
+    )
 
+    # extracellular carbondioxide dynamics
     dxdt[3] = 0.0
 
-    dxdt[4] = {
-        k1 * x[4] * MW_H / MW_CO2 - k2 * x[5] * x[6] * 1000 / (V_extracell * MW_HCO3) # chemical equilibrium
+    # extracellular hydrogen dynamics
+    dxdt[4] = (
+        k1 * x[3] * MW_H / MW_CO2 - k2 * x[4] * x[5] * 1000 / (V_extracell * MW_HCO3) # chemical equilibrium
 	    + nu_MCT_in_out - nu_MCT_out_in + nu_NHE_in_out + nu_CA9  * MW_H / MW_CO2 # extracellular hydrogen dynamics
-    }
-
-    dxdt[5] = {
-        k1 * x[4] * MW_HCO3 / MW_CO2 - k2 * x[5] * x[6] * 1000 / (V_extracell * MW_H) # chemical equilibrium
+    )
+    
+    # extracellular bicarbonate ions dynamics
+    dxdt[5] = (
+        k1 * x[3] * MW_HCO3 / MW_CO2 - k2 * x[4] * x[5] * 1000 / (V_extracell * MW_H) # chemical equilibrium
 		- nu_THCO3_out_in + nu_CA9 * MW_HCO3 / MW_CO2
-    }
+    )
     return(dxdt)
     
 def model (x, y, t, dt, VMAXAcL, VMAXNHE, VMAXTHCO3, VMAXCA9):
@@ -133,5 +136,16 @@ u87_file_path = "/home/spinicck/PhD/Data/alaa-experiment/U87_pH_time_regulation.
 f98_file_path = "/home/spinicck/PhD/Data/alaa-experiment/F98_pH_time_regulation.txt"
 u87_df = pd.read_table(u87_file_path, sep="\t", index_col=0)
 
+t_vect = numpy.linspace(0.0, 1.0, num=10000 )
+constant_rates = (9.58 * math.pow(10,-5), 5.15 * math.pow(10,-7),  2.02 * math.pow(10,-5), 9.47 * math.pow(10,-2) )
+sol = solve_ivp(ph_ode, [0.0, 1.0], x, t_eval=t_vect, args=constant_rates, method='Radau' )
 
-sol = solve_ivp(ph_ode, [0.0, 1.0], x, t_eval=numpy.linspace(0.0, 1.0, num=100000 ), args=(0.0057480000000000005, 3.0900000000000006e-05, 0.0012120000000000002, 5.6820000000000004) )
+pHi = []
+pHe = []
+for mHi in sol.y[1]: pHi.append( -math.log10(1000 * mHi / (V_cell * MW_H) ) )
+for mHe in sol.y[4]: pHe.append( -math.log10(1000 * mHe / (V_extracell * MW_H) ) )
+
+plt.plot(sol.t, pHi)
+plt.plot(sol.t, pHe)
+plt.legend(['pHi', 'pHe'])
+plt.show()
