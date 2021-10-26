ce4aaf83c008312fb5a8ed83e2a1ff434c72e9f9
Kévin SPINICCI <kevin.spinicci@outlook.com>
2021-10-26 13:01:17 +0200

    Changing unit

--- Simu-Piasentin/simu-plasentin-mol.cpp
+++ Simu-Piasentin/simu-plasentin-mol.cpp
@@ -126,26 +126,30 @@ int cell(const gsl_vector *x, void *params, gsl_vector *f)
     const double m_H_c = gsl_vector_get(x, 3);
     const double m_HCO3_c = gsl_vector_get(x, 4);
 
-    diff_CO2_in_out = PM_CO2 * (m_CO2_c_old / V_c-m_CO2_C / (4.0 / 3.0 * Pi * pow(r_C, 3.0))) * (4.0 * Pi * pow(r_C, 2.0));
-    nu_MCT_in_out = (2.0 - tanh(c2aH_slope * (-log10(1000 * m_H_C / (4.0 / 3.0 * Pi * pow(r_C, 3.0) * MW_H))) - c2aH_thr)) * VMAXAcL * MW_H / MW_AcL 
-        * (4.0 * Pi * pow(r_C, 2.0)) * m_H_C / ((4.0 / 3.0 * Pi * pow(r_C, 3.0)) * K_mAcL * MW_H / MW_AcL + m_H_C);
-    nu_MCT_out_in = (2.0 - tanh(a2cH_slope * (-log10(1000 * m_H_c / (V_c * MW_H))) - a2cH_thr)) * VMAXAcL * MW_H / MW_AcL 
-        * (4.0 * Pi * pow(r_C, 2.0)) * m_H_c / (V_c * K_mAcL * MW_H / MW_AcL + m_H_c);
-    nu_NHE_in_out = SensO2 * SensATP * 0.5 * (1.0 + ((-log10(1000 * m_H_c / (V_c * MW_H))) - pH0_NHE) / (l_NHE + abs((-log10(1000 * m_H_c / (V_c * MW_H))) - pH0_NHE))) 
-        * VMAXNHE * (4.0 * Pi * pow(r_C, 2.0)) * pow(m_H_C, a) / (pow(4.0 / 3.0 * Pi * pow(r_C, 3.0) * MW_H * K_mNHE / 1000, a) + pow(m_H_C, a));
-    nu_THCO3_out_in = SensATP * (0.5) * (1.0 + tanh(l_THCO3 * (-log10(1000 * m_H_c / (V_c * MW_H)) - pHe0_THCO3))) 
-        * (0.5) * (1.0 + tanh(g_THCO3 * (pHi0_THCO3 - (-log10(1000 * m_H_C / (4.0 / 3.0 * Pi * pow(r_C, 3.0) * MW_H)))))) 
-        * VMAXTHCO3 * (4.0 * Pi * pow(r_C, 2.0)) * m_HCO3_c / (V_c * K_mTHCO3 * MW_HCO3 / 1000 + m_HCO3_c);
-    nu_CA9 = (3.0 + 2.0 * tanh(-d_CA9 * SensO2)) * VMAXCA9 * 4.0 * Pi * pow(r_C, 2.0) * m_CO2_c_old / (V_c * K_mCA9 * MW_CO2 / 1000 + m_CO2_c_old);
+    double S_cell = (4.0 * Pi * pow(r_C, 2.0));
+    double V_cell = 4.0 / 3.0 * Pi * pow(r_C, 3.0); 
+    diff_CO2_in_out = PM_CO2 * ( m_CO2_c_old - m_CO2_C ) * S_cell;
+    nu_MCT_in_out = (2.0 - tanh(c2aH_slope * (-log10(m_H_C)) - c2aH_thr)) * VMAXAcL / MW_AcL * S_cell * m_H_C / ( K_mAcL + m_H_C);
+    nu_MCT_out_in = (2.0 - tanh(a2cH_slope * (-log10(m_H_c)) - a2cH_thr)) * VMAXAcL / MW_AcL * S_cell * m_H_c / (K_mAcL + m_H_c);
+    nu_NHE_in_out = 
+        SensO2 * SensATP * 
+        0.5 * (1.0 + ( (-log10(m_H_c)) - pH0_NHE) / (l_NHE + abs((-log10(m_H_c)) - pH0_NHE))) 
+        * VMAXNHE * S_cell * pow(m_H_C, a) / (pow(K_mNHE, a) + pow(m_H_C, a));
+    nu_THCO3_out_in = 
+        SensATP * 
+        (0.5) * (1.0 + tanh(l_THCO3 * (-log10(m_H_c) - pHe0_THCO3))) 
+        * (0.5) * (1.0 + tanh(g_THCO3 * (pHi0_THCO3 - (-log10(m_H_C))) ) ) 
+        * VMAXTHCO3 * S_cell * m_HCO3_c / (K_mTHCO3 + m_HCO3_c);
+    nu_CA9 = (3.0 + 2.0 * tanh(-d_CA9 * SensO2)) * VMAXCA9 * S_cell * m_CO2_c_old / (K_mCA9 + m_CO2_c_old);
     
     double coeff = 1;
 
     //intracellular carbondioxide dynamics
     const double y0 = m_CO2_C-m_CO2_C_old-dt * coeff * (
         //internalrate
-        SensO2 * q_O2 * MW_CO2 / MW_O2
+        SensO2 * q_O2 * 1e-12 / MW_O2
         //chemicalequilibrium
-        -k1 * m_CO2_C + k2 * m_H_C * m_HCO3_C * 1000 * MW_CO2 / (4.0 / 3.0 * Pi * pow(r_C, 3.0) * MW_H * MW_HCO3)
+        -k1 * m_CO2_C + k2 * m_H_C * m_HCO3_C
         //diffusion
         + diff_CO2_in_out
     );
@@ -155,7 +159,7 @@ int cell(const gsl_vector *x, void *params, gsl_vector *f)
         //internal rate
         SensATP * gAcL * MW_H / MW_AcL
         //chemical equilibrium
-        + k1 * m_CO2_C * MW_H / MW_CO2 - k2 * m_H_C * m_HCO3_C * 1000 / (4.0 / 3.0 * Pi * pow(r_C, 3.0) * MW_HCO3)
+        + k1 * m_CO2_C - k2 * m_H_C * m_HCO3_C
         //nu_MCT_in->out
         - nu_MCT_in_out
         //nu_MCT_out->in
@@ -167,7 +171,7 @@ int cell(const gsl_vector *x, void *params, gsl_vector *f)
     //intracellular bicarbonate ions dynamics
     const double y2 = m_HCO3_C - m_HCO3_C_old - dt * coeff * (
         //chemical equilibrium
-        k1 * MW_HCO3 / MW_CO2 * m_CO2_C - k2 * m_H_C * m_HCO3_C * 1000 / (4.0 / 3.0 * Pi * pow(r_C, 3.0) * MW_H)
+        k1 * m_CO2_C - k2 * m_H_C * m_HCO3_C
         //nu_THCO3_out->in
         + nu_THCO3_out_in
     );
@@ -175,7 +179,7 @@ int cell(const gsl_vector *x, void *params, gsl_vector *f)
     //extracellular hydrogen dynamics
     const double y3 = m_H_c - m_H_c_old - dt * coeff * (
         //chemical equilibrium
-        k1 * m_CO2_c_old * MW_H / MW_CO2 - k2 * m_H_c * m_HCO3_c * 1000 / (V_c * MW_HCO3)
+        k1 * m_CO2_c_old - k2 * m_H_c * m_HCO3_c
         //nu_MCT_in->out
         + nu_MCT_in_out
         //nu_MCT_out->in
@@ -183,17 +187,17 @@ int cell(const gsl_vector *x, void *params, gsl_vector *f)
         //nu_NHE_in->out 
         + nu_NHE_in_out
         //nu_CA9 
-        + nu_CA9 * MW_H / MW_CO2
+        + nu_CA9
     );
 
     //extracellular bicarbonate ions dynamics
     const double y4 = m_HCO3_c - m_HCO3_c_old - dt * coeff * (
         //chemical equilibrium
-        k1 * m_CO2_c_old * MW_HCO3 / MW_CO2 - k2 * m_H_c * m_HCO3_c * 1000 / (V_c * MW_H)
+        k1 * m_CO2_c_old - k2 * m_H_c * m_HCO3_c
         //nu_THCO3_out->in
         - nu_THCO3_out_in
         //nu_CA9
-        + nu_CA9 * MW_HCO3 / MW_CO2
+        + nu_CA9
     );
 
     gsl_vector_set(f, 0, y0);
@@ -312,33 +316,37 @@ int main(void)
     const double MW_O2 = 32.0;                  // g/mol
     const double MW_HCO3 = 61.0;                // g/mol
     const double MW_AcL = 90.1;                 // g/mol
-    const double r_C = 8.412711;                    // mim
-    const double PM_CO2 = 3.2 * pow(10, 4)*60;     // mim/s
-    const double gAcL = 3.8 * pow(10,-4)*60;       // pg/s
-    const double q_O2 = 3.5 * pow(10,-5)*60;       // pg/s
-    const double k1 = 0.144*60;                    // 1/s
-    const double k2 = 1.9 * pow(10, 5)*60;         // 1/(M*s)
-    const double VMAXAcL = 9.58 * pow(10,-5)*60;   // pg/(s*mim^2)
-    const double K_mAcL = 0.405 * pow(10,-3);   // pg/mim^3
+    const double r_C = 8.41;                    // mim
+
+    const double S_cell = (4.0 * Pi * pow(r_C, 2.0));
+    const double V_cell = 4.0 / 3.0 * Pi * pow(r_C, 3.0); 
+
+    const double PM_CO2 = 3.2 * pow(10, 4);     // mim/s
+    const double gAcL = 3.8 * pow(10,-4) * 1e-12 / MW_AcL;       // mol/s
+    const double q_O2 = 3.5 * pow(10,-5) * 1e-12 / MW_O2;       // mol/s
+    const double k1 = 0.144;                    // 1/s
+    const double k2 = 1.9 * pow(10, 5);         // 1/(M*s)
+    const double VMAXAcL = 9.58 * pow(10,-5)* 1e-12 / (MW_AcL * V_cell * 1e-15 );   // mol/(s*L*um2)
+    const double K_mAcL = 0.405 * pow(10,-3) * 1e-12 * 1e-15;   // mol/L
     const double a2cH_slope = 1.5;             // adim
     const double a2cH_thr = 7.0;               // adim
     const double c2aH_slope = 1.5;              // adim
     const double c2aH_thr = 7.0;                // adim
-    const double VMAXNHE = 5.15 * pow(10,-7)*60;   // pg/(s*mim^2)
-    const double K_mNHE = 0.196 * pow(10,-6);   // pg/mim^3
+    const double VMAXNHE = 5.15 * pow(10,-7) * 1e-12 / (4.0 / V_cell * 1e-15);   // mol/(s*L*um2)
+    const double K_mNHE = 0.196 * pow(10,-6);   // mol/L
     const double a = 2.67;                      // adim
     const double l_NHE = 0.076;                 // adim
     const double pH0_NHE = 7.1;                 // adim
-    const double VMAXTHCO3 = 2.02 * pow(10,-5)*60; // pg/(s*mim^2)
-    const double K_mTHCO3 = 7.38 * pow(10,-3);  // pg/mim^3
+    const double VMAXTHCO3 = 2.02 * pow(10,-5) * 1e-12 / ( MW_HCO3 * V_cell * 1e-15 ); // mol/(s*L*um2)
+    const double K_mTHCO3 = 7.38 * pow(10,-3);  // mol/L
     const double l_THCO3 = 1.63;                // adim
     const double pHe0_THCO3 = 6.85;             // adim
     const double g_THCO3 = 4.2;                 // adim
     const double pHi0_THCO3 = 6.90;             // adim
-    const double VMAXCA9 = 9.47 * pow(10,-2)*60;   // pg/(s*mim^2)
-    const double K_mCA9 = 7.2 * pow(10,-3);     // pg/mim^3
+    const double VMAXCA9 = 9.47 * pow(10,-2) * 1e-12 / ( V_cell * 1e-15 );   // mol/(s*L)
+    const double K_mCA9 = 7.2 * pow(10,-3);     // mol/L
     const double d_CA9 = 7.3;                   // adim
-    const double V_c = 2e7; // 1.0 * pow(10, 12);       // mim^3
+    const double V_c = 2e7;       // mim^3
     const double pKa = -log10(k1 / k2);          // adim
     const double pH_cell = 7.40;                // adim
 
@@ -353,13 +361,13 @@ int main(void)
     cin >> pH;
 
     // starting conditions
-    m_CO2_C_old = 5.39 * pow(10.0, -5) * (4.0 / 3.0 * Pi * pow(r_C, 3.0));
-    m_H_C_old = pow(10.0, -pH_cell - 3.0) * (4.0 / 3.0 * Pi * pow(r_C, 3.0));
-    m_HCO3_C_old = MW_HCO3 / MW_CO2 * m_CO2_C_old * pow(10.0, pH_cell - pKa);
+    m_CO2_C_old = 5.39 * pow(10.0, -5) * 1e-12 * 1e15 / MW_CO2;
+    m_H_C_old = pow(10.0, -pH_cell);
+    m_HCO3_C_old = m_CO2_C_old  * pow(10.0, pH_cell - pKa);
 
-    m_CO2_c_old = 5.39 * pow(10.0,-5) * V_c;
-    m_H_c_old = pow(10.0, -pH - 3.0) * V_c;
-    m_HCO3_c_old = MW_HCO3 / MW_CO2 * m_CO2_c_old * pow(10.0, pH - pKa);
+    m_CO2_c_old = 5.39 * pow(10.0, -5) * 1e-12 * 1e15 / MW_CO2;
+    m_H_c_old = pow(10.0, -pH );
+    m_HCO3_c_old = m_CO2_c_old * pow(10.0, pH - pKa);
 
     const size_t n = 5;
     struct cell_params cell_p = {
@@ -424,8 +432,8 @@ int main(void)
     // first term
     outData << "0" << "\t" << m_CO2_C_old << "\t" << m_H_C_old << "\t" << m_HCO3_C_old << "\t" << m_H_c_old << "\t" << m_HCO3_c_old << endl;
     outDatapH << "0" << "\t"
-        << -log10(1000 * m_H_C_old / (4.0 / 3.0 * Pi * pow(r_C, 3.0))) << "\t"
-        << -log10(1000 * m_H_c_old / V_c) << "\t" << log10((m_HCO3_C_old * MW_CO2 * k2) / (m_CO2_C_old * MW_HCO3 * k1))
+        << -log10(m_H_C_old) << "\t"
+        << -log10(m_H_c_old) << "\t" << log10((m_HCO3_C_old * MW_CO2 * k2) / (m_CO2_C_old * MW_HCO3 * k1))
         << "\t" << log10((m_HCO3_c_old * MW_CO2 * k2) / (m_CO2_c_old * MW_HCO3 * k1)) << endl;
     flux << "0" << "\t" <<  diff_CO2_in_out << "\t" << nu_MCT_in_out << "\t" << nu_MCT_out_in << "\t" << nu_NHE_in_out << "\t" << nu_THCO3_out_in << "\t" << nu_CA9 << "\t" << export_protons << "\t" << protons_exported << endl;
 
@@ -462,8 +470,8 @@ int main(void)
                 << "\t" << gsl_vector_get(s->x, 3) << "\t" << gsl_vector_get(s->x, 4) << endl;
 
             outDatapH << j * dt << "\t"
-                << -log10(1000 * gsl_vector_get(s->x, 1) / (4.0 / 3.0 * Pi * pow(r_C, 3.0))) << "\t"
-                << -log10(1000 * gsl_vector_get(s->x, 3) / V_c)
+                << -log10(gsl_vector_get(s->x, 1)) << "\t"
+                << -log10(gsl_vector_get(s->x, 3))
                 << "\t" << log10((gsl_vector_get(s->x, 2) * MW_CO2 * k2) / (gsl_vector_get(s->x, 0) * MW_HCO3 * k1))
                 << "\t" << log10((gsl_vector_get(s->x, 4) * MW_CO2 * k2) / (m_CO2_c_old * MW_HCO3 * k1)) << endl;
 
