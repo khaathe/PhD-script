41277a53ac3fd24243b40237168a3d62b9ea802e
Kévin SPINICCI <kevin.spinicci@outlook.com>
2021-11-08 12:39:18 +0100

    class for each model

--- hif-model/hif-model.py
+++ hif-model/hif-model.py
@@ -1,75 +1,13 @@
 #!/usr/bin
 
+from abc import ABC, abstractclassmethod
 import numpy as np
 from plotly.subplots import make_subplots
 from scipy.integrate import solve_ivp
 import plotly.graph_objects as go
 import math
 
-def H(y,s,n,gamma):
-    return s**n / ( s**n + y**n ) + gamma * y**n / (s**n + y**n)
-
-def base_model(t, x, p):
-    dxdt = [None] * 8
-    hif, ldh, pdk, pdh, o, g, atp, h = x
-    A, D, N, hif_coeff_prod, S, gamma, Vo, Ko, pg, A0,  Kg, Kh, L, beta, k, ldh0 = p.values()
-    Co = Vo * ( o/(o+Ko) )
-    Cg = ( (pg*A0/2.0) - (27.0*Co/10.0) ) * ( g/(g+Kg) )
-    Pa = ( (2.0 * Cg) + ( (27.0/5.0)*Co ) )
-    Ph = Kh*( (29.0*(pg*Vo-Co))/5.0 )
-    dxdt[0] = A * hif_coeff_prod - D * H(o, S[0][1], N, gamma[0][1]) * hif
-    dxdt[1] = A * H(hif, S[1][2], N, gamma[1][2]) - D * ldh
-    dxdt[2] = A * H(hif, S[1][3], N, gamma[1][3]) - D * pdk
-    dxdt[3] = A * H(pdk, S[3][4], N, gamma[3][4]) - D * pdh
-    dxdt[4] = -Co
-    dxdt[5] = -Cg
-    dxdt[6] = Pa
-    dxdt[7] = Ph
-    return dxdt
-
-def new_model(t, x, p):
-    dxdt = [None] * 8
-    hif, ldh, pdk, pdh, o, g, atp, h = x
-    A, D, N, hif_coeff_prod, S, gamma, Vo, Ko, pg, A0,  Kg, Kh, L, beta, k, ldh0 = p.values()
-    
-    pg = (L-beta)/(1+math.exp(-k*(ldh-ldh0))) + beta
-
-    Co = Vo * ( o/(o+Ko) )
-    Cg = ( (pg*A0/2.0) - (27.0*Co/10.0) ) * ( g/(g+Kg) )
-    Pa = ( (2.0 * Cg) + ( (27.0/5.0)*Co ) )
-    Ph = Kh * ( (29.0*(pg*Vo-Co))/5.0 )
-    dxdt[0] = A * hif_coeff_prod - D * H(o, S[0][1], N, gamma[0][1]) * hif
-    dxdt[1] = A * H(hif, S[1][2], N, gamma[1][2]) - D * ldh
-    dxdt[2] = A * H(hif, S[1][3], N, gamma[1][3]) - D * pdk
-    dxdt[3] = A * H(pdk, S[3][4], N, gamma[3][4]) - D * pdh
-    dxdt[4] = -Co
-    dxdt[5] = -Cg
-    dxdt[6] = Pa
-    dxdt[7] = Ph
-    return dxdt
-
-def o2_decreasing(t, x, p):
-    dxdt = [None] * len(x)
-    hif, ldh, pdk, pdh, o, g, atp, h, oConsumption, gConsumption = x
-    A, D, N, hif_coeff_prod, S, gamma, Vo, Ko, pg, A0,  Kg, Kh, L, beta, k, ldh0 = p.values()
-    
-    pg = (L-beta)/(1+math.exp(-k*(ldh-ldh0))) + beta
-    Co = Vo * ( o/(o+Ko) )
-    Cg = ( (pg*A0/2.0) - (27.0*Co/10.0) ) * ( g/(g+Kg) )
-    Pa = ( (2.0 * Cg) + ( (27.0/5.0)*Co ) )
-    Ph = Kh * ( (29.0*(pg*Vo-Co))/5.0 )
-    dxdt[0] = A * hif_coeff_prod - D * H(o, S[0][1], N, gamma[0][1]) * hif
-    dxdt[1] = A * H(hif, S[1][2], N, gamma[1][2]) - D * ldh
-    dxdt[2] = A * H(hif, S[1][3], N, gamma[1][3]) - D * pdk
-    dxdt[3] = A * H(pdk, S[3][4], N, gamma[3][4]) - D * pdh
-    dxdt[4] = -0.0009333333 if (t>(480-30) and t<(480+30) ) else 0.0
-    dxdt[5] = 0.0
-    dxdt[6] = Pa
-    dxdt[7] = Ph
-    dxdt[8] = Co
-    dxdt[9] = Cg
-    return dxdt
-class Simu:
+class Simu(ABC):
     NORMOXIA = 0.056 # ~5%
     HYPOXIA = 0.010564 # 1%
     ANOXIA = 1.0564e-05 # 0.001%
@@ -112,11 +50,7 @@ class Simu:
             "pg" : 1.0,
             "A0" : 29.0/5.0 * 2.1e-11,
             "Kg" : 0.04,
-            "Kh" : 2.5e-4,
-            "L" : 50,
-            "beta" : 1,
-            "k" : 10,
-            "ldh0" : 2.176675
+            "Kh" : 2.5e-4
         }
         self.pSimu = {
             "tspan" : [0.0, 1440.0],
@@ -125,9 +59,16 @@ class Simu:
             "subCols" : 1
         }
         self.solutions = {}
-        self.model = base_model
-        self.modelName = "Base Model"
+        self.model = self.model
+        self.modelName = ""
         self.description = ""
+
+    @abstractclassmethod
+    def model(self, t, x, p):
+        pass
+
+    def H(self,y,s,n,gamma):
+        return s**n / ( s**n + y**n ) + gamma * y**n / (s**n + y**n)
 
     def run(self):
         self.solutions= dict()
@@ -182,20 +123,6 @@ class Simu:
             col=col_index
         )
         return sub
-
-    def setModel(self, modelType):
-        if ( modelType.lower() == "base_model" ):
-            self.model = base_model
-            self.modelName = "Base Model"
-        elif ( modelType.lower() == "new_model" ):
-            self.model = new_model
-            self.modelName = "New Model"
-        elif ( modelType.lower() == "o2_decreasing" ):
-            self.model = o2_decreasing
-            self.modelName = "Model with O2 decreasing"
-        else:
-            self.model = base_model
-            self.modelName = "Base Model"
     
     def updateParamOde(self, newVal):
         self.pOde.update(newVal)
@@ -207,46 +134,130 @@ class Simu:
         self.initialCondition = initialConditions
         self.pSimu["subRows"] = len(self.initialCondition)
 
+class BaseModel(Simu):
+    def __init__(self):
+        super().__init__()
+        self.modelName = "Base Model"
+        self.description = ""
+    
+    def model(self, t, x, p):
+        dxdt = [None] * 8
+        hif, ldh, pdk, pdh, o, g, atp, h = x
+        A, D, N, hif_coeff_prod, S, gamma, Vo, Ko, pg, A0,  Kg, Kh = p.values()
+        Co = Vo * ( o/(o+Ko) )
+        Cg = ( (pg*A0/2.0) - (27.0*Co/10.0) ) * ( g/(g+Kg) )
+        Pa = ( (2.0 * Cg) + ( (27.0/5.0)*Co ) )
+        Ph = Kh*( (29.0*(pg*Vo-Co))/5.0 )
+        dxdt[0] = A * hif_coeff_prod - D * self.H(o, S[0][1], N, gamma[0][1]) * hif
+        dxdt[1] = A * self.H(hif, S[1][2], N, gamma[1][2]) - D * ldh
+        dxdt[2] = A * self.H(hif, S[1][3], N, gamma[1][3]) - D * pdk
+        dxdt[3] = A * self.H(pdk, S[3][4], N, gamma[3][4]) - D * pdh
+        dxdt[4] = -Co
+        dxdt[5] = -Cg
+        dxdt[6] = Pa
+        dxdt[7] = Ph
+        return dxdt
+
+class NewModel(Simu):
+    def __init__(self):
+        super().__init__()
+        self.pOde.update({
+            "L" : 50,
+            "beta" : 1,
+            "k" : 10,
+            "ldh0" : 2.176675
+        })
+        self.modelName = "New Model"
+        self.description = ""
+    
+    def model(self, t, x, p):
+        dxdt = [None] * 8
+        hif, ldh, pdk, pdh, o, g, atp, h = x
+        A, D, N, hif_coeff_prod, S, gamma, Vo, Ko, pg, A0,  Kg, Kh, L, beta, k, ldh0 = p.values()
+        pg = (L-beta)/(1+math.exp(-k*(ldh-ldh0))) + beta
+        Co = Vo * ( o/(o+Ko) )
+        Cg = ( (pg*A0/2.0) - (27.0*Co/10.0) ) * ( g/(g+Kg) )
+        Pa = ( (2.0 * Cg) + ( (27.0/5.0)*Co ) )
+        Ph = Kh * ( (29.0*(pg*Vo-Co))/5.0 )
+        dxdt[0] = A * hif_coeff_prod - D * self.H(o, S[0][1], N, gamma[0][1]) * hif
+        dxdt[1] = A * self.H(hif, S[1][2], N, gamma[1][2]) - D * ldh
+        dxdt[2] = A * self.H(hif, S[1][3], N, gamma[1][3]) - D * pdk
+        dxdt[3] = A * self.H(pdk, S[3][4], N, gamma[3][4]) - D * pdh
+        dxdt[4] = -Co
+        dxdt[5] = -Cg
+        dxdt[6] = Pa
+        dxdt[7] = Ph
+        return dxdt
+        
+
+class O2Decreasing(NewModel):
+    def __init__(self):
+        super().__init__()
+        self.initialCondition = {
+             "" : [ 3.565, 1.726, 3.31, 0.28, 0.056, 5.0, 0.0, 10**(-7.4)/1000, 0.0, 0.0]
+        }
+        self.pSimu.update({
+            "subRows" : 1,
+            "subCols" : 1
+        })
+        self.LABELS_PLOT.extend(["Oxygen Consumption", "Glucose Consumption"])
+        self.COLOR_PLOT.extend(["#7FFFD4", "#FFE4C4"])
+        self.modelName = "Model with Decreasing O2 over time"
+        self.description = ""
+    
+    def model(self, t, x, p):
+        dxdt = [None] * len(x)
+        hif, ldh, pdk, pdh, o, g, atp, h, oConsumption, gConsumption = x
+        A, D, N, hif_coeff_prod, S, gamma, Vo, Ko, pg, A0,  Kg, Kh, L, beta, k, ldh0 = p.values()
+        
+        pg = (L-beta)/(1+math.exp(-k*(ldh-ldh0))) + beta
+        Co = Vo * ( o/(o+Ko) )
+        Cg = ( (pg*A0/2.0) - (27.0*Co/10.0) ) * ( g/(g+Kg) )
+        Pa = ( (2.0 * Cg) + ( (27.0/5.0)*Co ) )
+        Ph = Kh * ( (29.0*(pg*Vo-Co))/5.0 )
+        dxdt[0] = A * hif_coeff_prod - D * self.H(o, S[0][1], N, gamma[0][1]) * hif
+        dxdt[1] = A * self.H(hif, S[1][2], N, gamma[1][2]) - D * ldh
+        dxdt[2] = A * self.H(hif, S[1][3], N, gamma[1][3]) - D * pdk
+        dxdt[3] = A * self.H(pdk, S[3][4], N, gamma[3][4]) - D * pdh
+        dxdt[4] = -0.0009333333 if (t>(480-30) and t<(480+30) ) else 0.0
+        dxdt[5] = 0.0
+        dxdt[6] = Pa
+        dxdt[7] = Ph
+        dxdt[8] = Co
+        dxdt[9] = Cg
+        return dxdt
+
 if __name__ == "__main__":
-    # simulation = Simu()
+    # simulation = BaseModel()
     # simulation.run()
     # simulation.description = "Vo={}".format( simulation.pOde["Vo"] )
     # simulation.plot()
 
-    # simulation = Simu()
+    # simulation = BaseModel()
     # simulation.setVo(1.16e-4)
     # simulation.description = "Vo={}".format( simulation.pOde["Vo"] )
     # simulation.run()
     # simulation.plot()
     
-    # simulation = Simu()
+    # simulation = BaseModel()
     # simulation.setVo(1.16e-4)
     # simulation.updateParamOde({"pg" : 50})
     # simulation.description = "Vo = {}, pg = {}".format(simulation.pOde["Vo"], simulation.pOde["pg"])
     # simulation.run()
     # simulation.plot()
 
-    # simulation = Simu()
-    # simulation.setModel("new_model")
+    # simulation = NewModel()
     # simulation.description = "Vo={}".format( simulation.pOde["Vo"] )
     # simulation.run()
     # simulation.plot()
 
-    # simulation = Simu()
-    # simulation.setModel("new_model")
+    # simulation = NewModel()
     # simulation.setVo(1.16e-4)
     # simulation.description = "Vo={}".format( simulation.pOde["Vo"] )
     # simulation.run()
     # simulation.plot()
 
-    simulation = Simu()
-    simulation.setInitialCondition({
-        "" : [ 3.565, 1.726, 3.31, 0.28, 0.056, 5.0, 0.0, 10**(-7.4)/1000, 0.0, 0.0]
-    })
-    simulation.LABELS_PLOT = ["HIF", "LDH", "PDK", "PDH", "Oxygen", "Glucose", "ATP", "H+", "Oxygen Consumption", "Glucose Consumption"]
-    simulation.COLOR_PLOT.append("#7FFFD4")
-    simulation.COLOR_PLOT.append("#FFE4C4")
-    simulation.description = "Vo={}".format( simulation.pOde["Vo"] )
-    simulation.setModel("o2_decreasing")
-    simulation.run()
-    simulation.plot()
+    # simulation = O2Decreasing()
+    # simulation.description = "Vo={}".format( simulation.pOde["Vo"] )
+    # simulation.run()
+    # simulation.plot()
